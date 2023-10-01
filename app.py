0470631b1baaf71d18c5cbdfd791f8237129ebf8
noahkarsky <noah.karsky8@gmail.com>
2023-10-01 18:29:44 -0500

    updates to app

--- app.py
+++ app.py
@@ -1,82 +1,107 @@
 import streamlit as st
 from streamlit_agraph import agraph, Config
-from data_import_formatting import import_and_format_data
+from data_import_formatting import import_and_format_data, make_wordcloud, make_word_count_bar_chart
 import yaml
 import networkx as nx
 
-with open('config.yaml', 'r') as file:
+from collections import Counter
+with open("config.yaml", "r") as file:
     config = yaml.safe_load(file)
 
-node_color_map = config['node_color_map']
-#data import and formatting
-nodes,edges = import_and_format_data('/data/graph_data.json')
+node_color_map = config["node_color_map"]
+# data import and formatting
+nodes, edges = import_and_format_data()
+
 
 #### FUNCTIONS ####
 def find_related_nodes_efficient(node_id, edges_df, degree=2):
-    G = nx.from_pandas_edgelist(edges_df, 'source', 'target')
+    G = nx.from_pandas_edgelist(edges_df, "source", "target")
     related = nx.single_source_shortest_path_length(G, node_id, cutoff=degree)
     return list(related.keys())
 
+
 # Function to perform the graph rendering
 def render_graph(selected_node, nodes, edges):
-    if selected_node == 'ALL':
+    if selected_node == "ALL(takes a second to load)":
         filtered_nodes = nodes
         filtered_edges = edges
+        physics = True
     else:
         related_nodes = find_related_nodes_efficient(selected_node, edges, degree=2)
-        filtered_nodes = nodes[nodes['id'].isin(related_nodes)]
-        filtered_edges = edges[edges['source'].isin(related_nodes) & edges['target'].isin(related_nodes)]
+        filtered_nodes = nodes[nodes["id"].isin(related_nodes)]
+        filtered_edges = edges[
+            edges["source"].isin(related_nodes) & edges["target"].isin(related_nodes)
+        ]
+        physics = False
 
-    nodes_list = filtered_nodes['Node'].tolist()
-    edges_list = filtered_edges['Edge'].tolist()
+    nodes_list = filtered_nodes["Node"].tolist()
+    edges_list = filtered_edges["Edge"].tolist()
 
-    config = Config(width=3000, 
-                    height=700, 
-                    directed=True,
-                    # nodeHighlightBehavior=True, 
-                    # highlightColor="#F7A7A6",
-                    collapsible=True, 
-                    physics=True, 
-                    hierarchical=False,
-                    staticGraphWithDragAndDrop=True,
-                    graphviz_layout=True,
-                    )
+    config = Config(
+        width=3000,
+        height=700,
+        directed=True,
+        # nodeHighlightBehavior=True,
+        # highlightColor="#F7A7A6",
+        collapsible=True,
+        physics=physics,
+        hierarchical=False,
+        graphviz_layout=True,
+    )
 
     return agraph(nodes=nodes_list, edges=edges_list, config=config)
 
 
-#APP DESIGN
-#make st wide
+# APP DESIGN
+# make st wide
 st.set_page_config(layout="wide")
-st.title('Anonymous Phenomena Timeline - Knowledge Graph')
-st.markdown('''
+st.title("Anonymous Phenomena Timeline - Knowledge Graph")
+st.markdown(
+    """
 
 This is a small project to turn [the author-unknown timeline of UAP historical events](https://pdfhost.io/v/gR8lAdgVd_Uap_Timeline_Prepared_By_Another), released during the [July 26 Congressional hearings](https://time.com/6298287/congress-ufo-hearing/), into a knowledge graph. 
 It helps to view the app in full screen, if not sometimes the nodes can be found dragging around the graph.**It may take several seconds for ALL nodes to render**
     
-    ''')
+    """
+)
+
+
 
 # Create two columns
-col1, col2 = st.columns([9, 1]) 
+col1, col2 = st.columns([9, 1])
 # Add a dropdown menu for node selection in the first column
 with col1:
     # Adding 'ALL' to the list of node IDs
-    node_options = ['ALL'] + nodes['id'].tolist()
-    selected_node = st.selectbox('Select a node', node_options)
+    node_options = ["ALL(takes a second to load)"] + nodes["id"].tolist()
+    selected_node = st.selectbox("Select a node", node_options)
 
 # Add a legend in the second column
 with col2:
     st.markdown("## Legend")
     for node_type, color in node_color_map.items():
-        st.markdown(f"<span style='color: {color};'>■</span> {node_type}", unsafe_allow_html=True)
+        st.markdown(
+            f"<span style='color: {color};'>■</span> {node_type}",
+            unsafe_allow_html=True,
+        )
 
 # APP FUNCTIONALITY (in the first column)
 with col1:
-   render_graph(selected_node, nodes, edges)
+    render_graph(selected_node, nodes, edges)
 
 
-st.markdown('''
 
+left_co,last_co = st.columns(2)
+with left_co:
+    st.plotly_chart(make_word_count_bar_chart(edges),use_container_width=True)
+    
+
+with last_co:
+    st.image(make_wordcloud(edges).to_array(),width=1200)
+    st.markdown("Wordcloud of the nodes in the timeline.",)
+
+st.markdown(
+    """
 Find the github repo [here](https://github.com/noahkarsky/anonymous_phenomena_timeline).
 Check out my other knowledge graph build off my own research here: [Project Amanita Knowledge Graph](https://noahkarsky.github.io/project-amanita/).
-''')
+"""
+)
--- data_import_formatting.py
+++ data_import_formatting.py
@@ -1,34 +1,139 @@
-from streamlit_agraph import agraph, Node, Edge, Config
+from streamlit_agraph import Node, Edge
 import pandas as pd
-# make a function to import data from a json file with the above format
-import yaml
-with open('config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
-
-node_color_map = config['node_color_map']
-
+from wordcloud import WordCloud
+import numpy as np
+from collections import Counter
 import json
-def import_json_data(json_file):
-    with open(json_file) as f:
-        data = json.load(f)
-    return data
+import yaml
+import plotly.express as px
 
 
-def import_and_format_data(json_file):
+def load_config(file_path: str = "config.yaml") -> dict:
+    with open(file_path, "r") as file:
+        config = yaml.safe_load(file)
+    return config
 
-    #data import and formatting
-    data = import_json_data('data/graph_data.json')
-    #make this into somethign graphistry can read
-    nodes = pd.DataFrame(data['nodes'])
-    edges = pd.DataFrame(data['edges'])
 
-    #correct the value 'groip' to 'group' found in the type column
-    nodes['type'] = nodes['type'].str.replace('groip', 'group')
-
-    #add color to nodes df
-    nodes['color'] = nodes['type'].map(node_color_map)
-    nodes['Node']  = nodes.apply(lambda x: Node(id=x['id'], label=x['node_edited'], size=10,shape='dot' ,color =x['color']), axis=1)
-    edges['Edge'] = edges.apply(lambda x: Edge(source=x['source'], target=x['target'], label=x['relationship_type']), axis=1)
+def create_mapping_dict(mapping_dict_path:str):
+    """Create a mapping dictionary from a json file"""
+    with open(mapping_dict_path) as json_file:
+        mapping_dict = json.load(json_file)
+    flattened = [(col, orig, altered) for col in mapping_dict for orig, altered in mapping_dict[col].items()]
     
+    mapping_dict = pd.DataFrame(flattened, columns=['column', 'original', 'altered'])
+    #drop the column column
+    mapping_dict = mapping_dict.drop(columns='column')
+    return mapping_dict
+
+mapping_dict = create_mapping_dict(r'D:\repos\anonymous_phenomena_timeline\data\mapping_dict.json')
+config = load_config()
+node_color_map = config["node_color_map"]
+
+def get_original(mapping_dict: pd.DataFrame, input_str: str) -> str:
+    #first we need to replac
+    match = mapping_dict[mapping_dict['altered'] == input_str]['original']
+    if not match.empty:
+        return match.values[0]
+    else:
+        return input_str
+
+
+def import_data(json_file: str) -> dict:
+    try:
+        with open(json_file) as f:
+            data = json.load(f)
+        
+        #clean up the data, we want to replace the node_edited values with the original values
+        #first replace spaces with underscores
+        data['nodes'] = [{k: v.replace(' ', '_') if k == 'node_edited' else v for k, v in d.items()} for d in data['nodes']]
+        #now use mapping dict to replace values
+        data['nodes'] = [{k: get_original(mapping_dict, v) if k == 'node_edited' else v for k, v in d.items()} for d in data['nodes']]
+        return data
+    except FileNotFoundError:
+        print(f"The file {json_file} was not found.")
+        return None  # Or raise the error to halt execution with raise
+
+
+def format_nodes(data: dict) -> pd.DataFrame:
+    nodes = pd.DataFrame(data["nodes"])
+    nodes["type"] = nodes["type"].str.replace("groip", "group")
+    nodes["color"] = nodes["type"].map(node_color_map)
+    nodes["Node"] = nodes.apply(
+        lambda x: Node(
+            id=x["id"], label=x["node_edited"], size=10, shape="dot", color=x["color"]
+        ),
+        axis=1,
+    )
+    #use mapping dict to replace values
+   
+
+    return nodes
+
+
+def format_edges(data: dict) -> pd.DataFrame:
+    edges = pd.DataFrame(data["edges"])
+    edges["Edge"] = edges.apply(
+        lambda x: Edge(
+            source=x["source"], target=x["target"], label=x["relationship_type"]
+        ),
+        axis=1,
+    )
+    return edges
+
+
+def import_and_format_data(
+    json_file: str = r"D:\repos\anonymous_phenomena_timeline\data\graph_data.json",
+) -> tuple:
     
+    data = import_data(json_file)
+    if data is None:
+        raise FileNotFoundError(f"The file {json_file} was not found.")
+    nodes = format_nodes(data)
+    edges = format_edges(data)
     return nodes, edges
+
+
+def aggregate_words(edges: pd.DataFrame) -> list:
+    words = []
+    for i in edges["source"]:
+        words.append(i)
+    for i in edges["target"]:
+        words.append(i)
+    #clean words
+    words = [word.replace('_', ' ') for word in words]
+    #now use mapping dict to replace values
+    words = [get_original(mapping_dict, word) for word in words]
+    return words
+
+
+def make_word_count_bar_chart(edges: pd.DataFrame):
+    words = aggregate_words(edges)
+    word_counts = Counter(words)
+    word_counts = dict(word_counts.most_common(20))
+    word_counts = pd.DataFrame.from_dict(word_counts, orient="index").reset_index()
+    word_counts.columns = ["word", "count"]
+    fig = px.bar(word_counts, x="word", y="count")
+    fig.update_layout(
+        title="Most Common Nodes in the Timeline",
+        xaxis_title="Word",
+        yaxis_title="Count",
+    )
+    return fig
+
+
+def color_func(word, font_size, position, orientation, random_state=None, **kwargs):
+    return "hsl(220, 100%%, %d%%)" % np.random.randint(50, 70)
+
+
+def make_wordcloud(edges: pd.DataFrame) -> WordCloud:
+    words = aggregate_words(edges)
+    frequency_dict = dict(Counter(words))
+    wordcloud = WordCloud(
+        background_color="white",
+        width=800,
+        height=400,
+        max_font_size=40,
+        color_func=color_func,
+    )
+    wordcloud.generate_from_frequencies(frequency_dict, max_font_size=33)
+    return wordcloud
